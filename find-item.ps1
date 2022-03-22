9bb8bfa2815c23e702627c67d96ca0c1489bc09c
joerickicki <85720947+joerickicki@users.noreply.github.com>
2022-03-21 17:18:58 -0700

    Update find-item.ps1

--- find-item.ps1
+++ find-item.ps1
@@ -1,8 +1,8 @@
 
 $FilterCriteria = @(
-    @{Filter = "Equals"; Field = "_templatename"; Value = "Product Part"},
-    @{Filter = "StartsWith"; Field = "_fullpath"; Value = "/sitecore/content/Home/Products/Fusible Switches and Panels" },
-    @{Filter = "Equals"; Field = "Banner Heading"; Value = $null; Invert=$true}
+    @{Filter = "Equals"; Field = "_templatename"; Value = "Product"},
+    @{Filter = "StartsWith"; Field = "_fullpath"; Value = "/sitecore/content/Home/Products/Product1" },
+    @{Filter = "Equals"; Field = "Product Description"; Value = $null; Invert=$true}
 )
 
 
